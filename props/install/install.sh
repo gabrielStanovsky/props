922bc7e99cba44a5ed5a95c51b05e511149b99da
ObviouslyNot <ObviouslyStillNot@gmail.com>
2017-03-16 13:16:32 +0100

    fixed outdated links to Berkeley Parser

--- props/install/install.sh
+++ props/install/install.sh
@@ -8,8 +8,8 @@ mkdir ./berkeleyparser/
 wget https://github.com/emcnany/berkeleyinterface/archive/master.zip -O ./install/berekeleyinterface.zip
 wget https://github.com/astraw/svg_stack/archive/master.zip -O install/svg_stack.zip
 wget "http://downloads.sourceforge.net/project/jpype/JPype/0.5.4/JPype-0.5.4.2.zip?r=&ts=1459330071&use_mirror=nchc" -O install/JPype-0.5.4.2.zip
-wget http://berkeleyparser.googlecode.com/files/eng_sm6.gr -O ./berkeleyparser/eng_sm6.gr
-wget http://berkeleyparser.googlecode.com/files/BerkeleyParser-1.7.jar -O ./berkeleyparser/BerkeleyParser-1.7.jar
+wget "https://github.com/slavpetrov/berkeleyparser/raw/master/eng_sm6.gr" -O ./berkeleyparser/eng_sm6.gr
+wget "https://github.com/slavpetrov/berkeleyparser/raw/master/BerkeleyParser-1.7.jar" -O ./berkeleyparser/BerkeleyParser-1.7.jar
 wget https://repo1.maven.org/maven2/edu/stanford/nlp/stanford-corenlp/3.3.1/stanford-corenlp-3.3.1.jar -O ./dependency_tree/stanford-corenlp-3.3.1.jar
 cd install/
 unzip -o svg_stack.zip 
